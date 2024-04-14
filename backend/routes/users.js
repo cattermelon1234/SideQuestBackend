2694b45d8143adb10d040ea6114cf5e2195ad2af
rybisteak <lidylan@seas.upenn.edu>
2024-04-13 21:41:09 -0400

    auth

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,30 +1,38 @@
 const express = require('express')
 //const User = require('../models/userModel')
 const {
-  createUser,
-  getUsers,
-  getUser,
-  deleteUser,
-  updateUser
+  //createUser,
+  //getUsers,
+  //getUser,
+  //deleteUser,
+  //updateUser,
+  loginUser,
+  signupUser
 } = require('../controllers/userController')       // importing all functions
 
 //creates an instance of the router
 const router = express.Router()
 
 // GET ALL USERS
-router.get('/', getUsers)
+//router.get('/', getUsers)
 
 // GET a single workout
-router.get('/:id', getUser)
+//router.get('/:id', getUser)
 
 // POST a new workout
-router.post('/', createUser)
+//router.post('/', createUser)
 
 // DELETE a new workout
-router.delete('/:id', deleteUser)
+//router.delete('/:id', deleteUser)
 
 // UPDATE a new workout
-router.patch('/:id', updateUser)
+//router.patch('/:id', updateUser)
+
+// login route
+router.post('/login', loginUser)
+
+// signup route
+router.post('/signup', signupUser)
 
 //we export the router at the end
 module.exports = router
